4e58df251b0a57a532b5ad71c27a82f5e5a4897f
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-07-05 01:03:38 -0300

    Model creation

---
+++ src/main/java/br/com/cesarfcg/productscrud/Entity/ProductEntity.java
@@ -0,0 +1,23 @@
+package br.com.cesarfcg.productscrud.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+}
