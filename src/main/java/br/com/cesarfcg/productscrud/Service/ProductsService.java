fd1a2b6fbcf28b639b305722566afb810282e5e1
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-07-06 21:50:32 -0300

    list and create SERVICE and h2-database configuration

---
+++ src/main/java/br/com/cesarfcg/productscrud/Service/ProductsService.java
@@ -0,0 +1,23 @@
+package br.com.cesarfcg.productscrud.Service;
+
+import br.com.cesarfcg.productscrud.Entity.ProductEntity;
+import br.com.cesarfcg.productscrud.Repository.ProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductsService {
+    private final ProductsRepository productsRepository;
+    public ProductsService(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
+    public List<ProductEntity> list(){
+        return productsRepository.findAll();
+    }
+    public List<ProductEntity > create(ProductEntity product){
+        productsRepository.save(product);
+        return list();
+    }
+}
