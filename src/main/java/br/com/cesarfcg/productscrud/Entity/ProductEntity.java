174a54bb048bbd1249d606a0d0b92e6a46a645f6
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-07-11 16:57:48 -0300

    Renomeando as colunas E table usando JPA

--- src/main/java/br/com/cesarfcg/productscrud/Entity/ProductEntity.java
+++ src/main/java/br/com/cesarfcg/productscrud/Entity/ProductEntity.java
@@ -8,22 +8,34 @@ import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.boot.context.properties.bind.Name;
 
 @Getter
 @Setter
 @ToString
 @Entity
-
+@Table(name = "TB_PRODUCTS")
 public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PRO_ID")
     private Long id;
+
     @NotBlank
+    @Column(name = "PRO_NAME")
     private String name;
-    @NotNull
+
+    @NotBlank
+    @Column(name = "PRO_DESCRIPTION")
     private String description;
+
     @NotNull
     @PositiveOrZero
+    @Column(name = "PRO_PRICE")
     private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_CATEGORIA")
+    private CategoryEntity category;
 
 }
