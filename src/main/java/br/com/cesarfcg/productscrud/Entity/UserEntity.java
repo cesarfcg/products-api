dcdc25446907019e8efbe33b9de38bda5b5f8e0b
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-07-15 19:42:30 -0300

    Criando a tabela de users

---
+++ src/main/java/br/com/cesarfcg/productscrud/Entity/UserEntity.java
@@ -0,0 +1,28 @@
+package br.com.cesarfcg.productscrud.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+@Entity
+@Getter
+@Setter
+@Table(name = "TB_USER")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long id;
+    @Column(name = "USER_NAME")
+    private String name;
+    @Column(name = "USER_LOGIN")
+    private String login;
+    @Column(name = "USER_PASSWORD")
+    private String password;
+
+}
